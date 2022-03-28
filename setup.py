d8db6d5c593de500e866cbecc9c988258aa8c6ef
Dariusz Antoniuk <qaston@gmail.com>
2022-03-28 18:50:42 +0100

    add deposit as a public module

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ THIS IS A STUB FOR RUNNING THE APP
 setup(
     name="staking_deposit",
     version='2.0.0',
-    py_modules=["staking_deposit"],
+    py_modules=["staking_deposit", "staking_deposit.deposit"],
     packages=find_packages(exclude=('tests', 'docs')),
     python_requires=">=3.7,<4",
 )
